4f712190ebf12266cf908b7b548c6eb720932543
boniseixas <bonisousa@live.com>
2023-09-08 22:22:55 -0300

    First commit

---
+++ spring-ioc-di-beans-autowired/src/main/java/seso/spring/SpringIocDiBeansAutowiredApplication.java
@@ -0,0 +1,25 @@
+package seso.spring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import seso.spring.app.ConversorJson;
+import seso.spring.app.ViaCepResponse;
+
+@SpringBootApplication
+public class SpringIocDiBeansAutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocDiBeansAutowiredApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça da Sé\",\"localidade\": \"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
+	}
+
+}
